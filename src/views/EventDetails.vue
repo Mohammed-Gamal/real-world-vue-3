209dc6a932926092263a53835f511b9006b6a151
Mohamed Gamal <1Mohamed.Gamal54@gmail.com>
2022-09-07 19:09:11 +0200

    Fixed a typo

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
-    <p>{{ event.time }} on {{ event.data }} @ {{ event.location }}</p>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
   </div>
 </template>
