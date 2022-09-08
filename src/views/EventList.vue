632720f3c72f4106e87d82b2a72b455e609db4d4
Mohamed Gamal <1Mohamed.Gamal54@gmail.com>
2022-09-08 12:13:16 +0200

    Initial commit

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -13,6 +13,8 @@ import EventService from '@/services/EventService.js'
 export default {
   name: 'EventList',
 
+  props: ['page'],
+
   components: {
     EventCard,
   },
@@ -24,7 +26,7 @@ export default {
   },
 
   created() {
-    EventService.getEvents()
+    EventService.getEvents(2, this.page)
       .then((response) => {
         this.events = response.data
       })
