719d01ca97a7f5773a4a55a271abd506cefe250e
Mohamed Gamal <1Mohamed.Gamal54@gmail.com>
2022-09-16 15:06:29 +0200

    Error Handling

--- src/store/index.js
+++ src/store/index.js
@@ -26,17 +26,17 @@ export default createStore({
           commit('ADD_EVENT', event)
         })
         .catch((error) => {
-          console.log(error)
+          throw error
         })
     },
 
     fetchEvents({ commit }) {
-      EventService.getEvents()
+      return EventService.getEvents()
         .then((response) => {
           commit('SET_EVENTS', response.data)
         })
         .catch((error) => {
-          console.log(error)
+          throw error
         })
     },
 
@@ -46,12 +46,12 @@ export default createStore({
       if (existingEvent) {
         commit('SET_EVENT', existingEvent)
       } else {
-        EventService.getEvent(id)
+        return EventService.getEvent(id)
           .then((response) => {
             commit('SET_EVENT', response.data)
           })
           .catch((error) => {
-            console.log(error)
+            throw error
           })
       }
     },
